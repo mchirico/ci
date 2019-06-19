2303fc95953c77b468a16d2fd6985c8ba69aecd8
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-06-19 08:07:17 -0400

    root

--- cmd/root.go
+++ cmd/root.go
@@ -34,23 +34,28 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	/*
-	Use:   "ci",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+		Use:   "ci",
+		Short: "A brief description of your application",
+		Long: `A longer description that spans multiple lines and likely contains
+	examples and usage of using your application. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Cobra is a CLI library for Go that empowers applications.
+	This application is a tool to generate the needed files
+	to quickly create a Cobra application.`,
 
-	 */
+	*/
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
 
-	RunE: func(cmd *cobra.Command, args []string)  error {
+	RunE: func(cmd *cobra.Command, args []string) error {
 
-		fmt.Printf("\n\n Start\n\n")
+		fmt.Printf(`
+   To create or overwrite the ci directory:
+         
+           ci create
+
+`)
 		return nil
 	},
 }
