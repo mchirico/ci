079fbd9071a0d9cd0d869d5c937da904f605c54b
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-06-19 21:12:01 -0400

    update msg

--- userMsg/usermsg.go
+++ userMsg/usermsg.go
@@ -3,7 +3,8 @@ package userMsg
 func NotFound() string {
 	s := `
    You must be in a git repo project directory
-   for this command to run. 
+   for this command to run. The files created from
+   "ci create" will need to be checked into the repo.
 
    git clone <your github project>
    cd <your github project>
@@ -11,6 +12,12 @@ func NotFound() string {
    Once you're in the directory you can run ci create
 
    ci create
+
+   Now add these files.
+
+   git add ci
+   git commit 'adding ci'
+   git push
 
 `
 	return s
